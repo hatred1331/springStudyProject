d17052962e51dfc30b413dd998592ce5f3fffc87
hatred1331 <degtyarev.av@gmail.com>
2024-10-20 18:35:59 +0300

    Add MainController and MainPage.html

--- demo/src/main/java/com/website/webblog/controllers/MainController.java
+++ demo/src/main/java/com/website/webblog/controllers/MainController.java
@@ -1,2 +1,17 @@
-package com.website.webblog.controllers;public class MainController {
+package com.website.webblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Main page");
+        return "home";
+    }
+
 }
